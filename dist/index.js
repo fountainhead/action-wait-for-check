9c1b26d35f736c257c2b3e61be42dfeb278337e9
bakoontz2 <brian.koontz@aa.com>
2022-08-25 11:50:10 -0500

    more logging

--- dist/index.js
+++ dist/index.js
@@ -3241,7 +3241,7 @@ exports.poll = (options) => __awaiter(void 0, void 0, void 0, function* () {
             repo,
             ref
         });
-        log(`${result}`);
+        log(JSON.stringify(result,null,2));
         log(`Retrieved ${result.data.check_runs.length} check runs named ${checkName}`);
         const completedCheck = result.data.check_runs.find(checkRun => checkRun.status === 'completed');
         if (completedCheck) {
